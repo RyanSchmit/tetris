adb9a56335a5e36800fdaf1e633a9f2d13e1c249
RyanSchmit <ryan15schmitt@gmail.com>
2020-06-03 12:27:26 -0400

    Changed colors

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const grid = document.querySelector('.grid')
     let squares = (document.querySelectorAll(".grid div"))
-    const ScoreDisplay = document.querySelector("#score")
-    const StartBtn = document.querySelector("#start-button")
+    const scoreDisplay = document.querySelector("#score")
+    const startBtn = document.querySelector("#start-button")
     const width = 10
  
     //Shapes of Terominoes
 
     const LTerominoes = [
         [1, width+1, width*2+1, 2],
-        [width, width+1, width*2, width*2+2], 
-        [1, width+1, width*2+1, width+2]
-        [width, width+2, width*2+1, width*2+2]
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
     ]
 
     const ITerominoes = [
@@ -88,15 +88,17 @@ document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("keyup", control)
 
     //moving tetrominoes down
-    let speed = 1000
+    var speed = 500
     
-    timerId = setInterval(moveDown, speed)
+    let timerId 
+    let score = 0
 
     function moveDown () {
         undraw()
         currentPosition += width
         draw()
         freeze()
+        gameOver()
     }
 
     //freeze tetrominoes
@@ -107,6 +109,7 @@ document.addEventListener("DOMContentLoaded", () => {
             current = theTerominoes[randomS][0]
             currentPosition = 4
             draw()
+            addScore()
         }
     }
 
@@ -149,6 +152,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //speed up falling 
     function fallFast() {
-        speed -= 100
+        speed = 100
     }
+
+    //start button function
+    startBtn.addEventListener("click", () => {
+        if (timerId) {
+            clearInterval(timerId)
+            timerId = null
+        } else {
+            draw()
+            timerId = setInterval(moveDown, speed)
+        }
+    })
+
+    //adding score 
+    function addScore() {
+        for (let i = 0; i < 199; i += width) {
+            const row =  [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+            if (row.every(index => squares[index].classList.contains("taken"))) {
+                score += 10
+                scoreDisplay.innerHTML = score 
+                row.forEach(index => {
+                    squares[index].classList.remove("taken")
+                    squares[index].classList.remove('tetromino')
+                })
+                const squaresRemoved = squares.splice(i, width)
+                squares = squaresRemoved.concat(squares)
+                squares.forEach(cell => grid.appendChild(cell))
+            }
+            
+        }
+    }
+
+    //end game
+    function gameOver() {
+        if(current.some(index => squares[currentPosition + index].classList.contains("taken"))) {
+            alert("Game Over")
+            clearInterval(timerId)
+        }
+    }
+    console.log(speed)
 })
